fb83da154d3e65e2901aa57a978de4d92a100181
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-26 21:00:00 +0500

    autho modular approach

--- training/controllers/controller.js
+++ training/controllers/controller.js
@@ -1,4 +1,4 @@
-
+const mongodb= require('../db/connectDb.js');
 
 
 const printName= (req, res)=>{
@@ -7,5 +7,14 @@ const printName= (req, res)=>{
 const story= (req, res)=>{
 res.send('Story telling is interesting game');
 };
+const getAll= async (req, res)=>{
+ 
+    const results= await mongodb.getDatabase().db().collection('student').find();
+    results.toArray().then((lists)=>{
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
+  
+   };
 
-module.exports={printName, story};
+module.exports={printName, story, getAll};
--- training/routes/router.js
+++ training/routes/router.js
@@ -5,5 +5,6 @@ const controller= require('../controllers/controller.js');
 
 router.get('/', controller.printName);
 router.get('/story', controller.story);
+router.get('/students', controller.getAll);
 
 module.exports= router;
