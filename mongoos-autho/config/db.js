394398cac0139e30a059bece48d11e70afeb7f04
waheednazeer <waheed.nazeer@hotmail.com>
2023-06-27 00:05:12 +0500

    google auth api created

--- mongoos-autho/config/db.js
+++ mongoos-autho/config/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose')
 
-const connectDB = async ()=>{
-    try{
-        const conn= await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-            
-        })
-        console.log(`ConnectDB connected: ${conn.connection.host}`)
-    }catch(error){
-        console.log(error)
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useFindAndModify: false,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 }
 
-module.exports= connectDB
+module.exports = connectDB
